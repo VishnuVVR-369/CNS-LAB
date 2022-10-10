489bbca168d45204c4f4684a1db4213edf7efd6a
VishnuVVR-369 <vishnuvardhanganji@gmail.com>
2022-10-10 21:42:30 +0530

    Completed Task-5

---
+++ TASK-5/Task-5.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+class RCA4 {
+    public static void main(String args[]) throws IOException {
+        int temp = 0;
+        String ptext;
+        String key;
+        int s[] = new int[256];
+        int k[] = new int[256];
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("\n ENTER PLAIN TEXT\t");
+        ptext = in.readLine();
+        System.out.println("\n\nENTER KEY TEXT\t\t");
+        key = in.readLine();
+        char ptextc[] = ptext.toCharArray();
+        char keyc[] = key.toCharArray();
+        int cipher[] = new int[ptext.length()];
+        int decrypt[] = new int[ptext.length()];
+        int ptexti[] = new int[ptext.length()];
+        int keyi[] = new int[key.length()];
+        for (int i = 0; i < ptext.length(); i++) {
+            ptexti[i] = (int) ptextc[i];
+        }
+        for (int i = 0; i < key.length(); i++) {
+            keyi[i] = (int) keyc[i];
+        }
+        for (int i = 0; i < 255; i++) {
+            s[i] = i;
+            k[i] = keyi[i % key.length()];
+        }
+        int j = 0;
+        for (int i = 0; i < 255; i++) {
+            j = (j + s[i] + k[i]) % 256;
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+        int i = 0;
+        j = 0;
+        int z = 0;
+        for (int l = 0; l < ptext.length(); l++) {
+            i = (l + 1) % 256;
+            j = (j + s[i]) % 256;
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            z = s[(s[i] + s[j]) % 256];
+            cipher[l] = z ^ ptexti[l];
+            decrypt[l] = z ^ cipher[l];
+        }
+        System.out.println("\n\nENCRYPTED:\t\t");
+        display(cipher);
+        System.out.println("\n\nDECRYPTED:\t\t");
+        display(decrypt);
+    }
+
+    static void display(int disp[]) {
+        char convert[] = new char[disp.length];
+        for (int l = 0; l < disp.length; l++) {
+            convert[l] = (char) disp[l];
+            System.out.print(convert[l]);
+        }
+    }
+
+}
